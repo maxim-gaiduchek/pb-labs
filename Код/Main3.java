8f9c49ba0f858d1236f8ebc0e42c506f42417546
sinfulsamurai <maxim.gayduchek@gmail.com>
2021-10-02 00:49:08 +0300

    Редаговано код

--- Код/Main3.java
+++ Код/Main3.java
@@ -1,18 +1,18 @@
 public class Main3 {
 
     public static void main(String[] args) {
-        float x = 0.56f, error = 0.0001f;
-        double sum = 0, element;
+        double x = 0.56, error = 1.0e-4;
+        double sum = 0, element = 0;
         int n = 1;
 
-        while (Math.abs(element = function(x, n++)) > error) {
+        while (Math.abs(element - (element = function(x, n++))) > error) {
             sum += element;
         }
 
         System.out.println(sum);
     }
 
-    private static double function(float x, int n) {
+    private static double function(double x, int n) {
         return (n % 2 == 0 ? 1 : -1) * ((Math.pow(x, 2 * n) + 1) / (Math.pow(2, n) + 1));
     }
 }

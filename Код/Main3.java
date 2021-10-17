8fc3562db5e3e878f15bd449d9be8bc675979053
sinfulsamurai <maxim.gayduchek@gmail.com>
2021-10-17 15:57:07 +0300

    Редаговано код. Створено звіт та блок-схему алгоритму

--- Код/Main3.java
+++ Код/Main3.java
@@ -1,18 +1,21 @@
 public class Main3 {
 
     public static void main(String[] args) {
-        double x = 0.56, error = 1.0e-4;
-        double sum = 0, element = 0;
-        int n = 1;
+        final double x = 0.56, error = 1.0e-4;
+        int n = 0;
+        double sum = 0, element = function(x, n++), diff = element;
 
-        while (Math.abs(element - (element = function(x, n++))) > error) {
+        while (Math.abs(diff) > error) {
             sum += element;
+            diff = element;
+            element = function(x, n++);
+            diff -= element;
         }
 
         System.out.println(sum);
     }
 
     private static double function(double x, int n) {
-        return (n % 2 == 0 ? 1 : -1) * ((Math.pow(x, 2 * n) + 1) / (Math.pow(2, n) + 1));
+        return (n % 2 == 0 ? 1 : -1) * (Math.pow(x, 2 * n) + 1) / (Math.pow(2, n) + 1);
     }
 }

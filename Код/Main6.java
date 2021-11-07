e04b0a51add7916d17d860152be759234c5c781c
sinfulsamurai <maxim.gayduchek@gmail.com>
2021-11-07 15:55:46 +0200

    Створено звіт та редаговано код

--- Код/Main6.java
+++ Код/Main6.java
@@ -5,22 +5,23 @@ public class Main6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a: ");
-        int a = scanner.nextInt();
-
-        System.out.print("Enter b: ");
-        int b = scanner.nextInt();
-
-        System.out.print("Enter c: ");
-        int c = scanner.nextInt();
+        int a = input(scanner, "Enter a: ");
+        int b = input(scanner, "Enter b: ");
+        int c = input(scanner, "Enter c: ");
 
         scanner.close();
 
         System.out.println(gcd(gcd(a, b), c));
     }
 
+    private static int input(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+
+        return scanner.nextInt();
+    }
+
     private static int gcd(int a, int b) {
-        while(a % b != 0 && b % a != 0) {
+        while (a % b != 0 && b % a != 0) {
             if (a > b) {
                 a %= b;
             } else {

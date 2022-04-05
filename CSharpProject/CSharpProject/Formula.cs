80c8640b8d40c62d4f1f4116c946a7dbf0ece2ce
maxim-gaiduchek <maxim.gayduchek@gmail.com>
2022-04-05 13:07:13 +0300

    Розроблено код на C#

---
+++ CSharpProject/CSharpProject/Formula.cs
@@ -0,0 +1,29 @@
+﻿namespace CSharpProject;
+
+public class Formula
+{
+    private double[] _coefficients;
+
+    public Formula(double a3, double a2, double a1, double a0)
+    {
+        _coefficients = new[] {a3, a2, a1, a0};
+    }
+
+    public double GetValue(double x)
+    {
+        // return _coefficients.Select((t, i) => t * Math.Pow(x, i)).Sum();
+        double result = 0;
+
+        for (var i = 0; i < _coefficients.Length; i++)
+        {
+            result += _coefficients[i] * Math.Pow(x, i);
+        }
+
+        return result;
+    }
+
+    public override string ToString()
+    {
+        return string.Join(" + ", _coefficients.Select((d, i) => (d < 0 ? $"({d})" : d.ToString()) + " * x^" + i));
+    }
+}
---
+++ CSharpProject/CSharpProject/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace CSharpProject;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var formulas = CreateFormulas();
+        
+        OutputArray(formulas);
+
+        var (a, b, eps) = InputRange();
+        var min = FindMinFormula(formulas, a, b, eps);
+
+        Console.WriteLine(min);
+    }
+
+    private static Formula[] CreateFormulas()
+    {
+        var random = new Random();
+        var length = random.Next(2, 10);
+        var formulas = new Formula[length];
+
+        for (var i = 0; i < length; i++)
+        {
+            formulas[i] = new Formula(
+                random.Next(-10, 10) * random.NextDouble(),
+                random.Next(-10, 10) * random.NextDouble(),
+                random.Next(-10, 10) * random.NextDouble(),
+                random.Next(-10, 10) * random.NextDouble()
+            );
+            formulas[i] = new Formula(
+                random.Next(-10, 10) * random.NextDouble(),
+                random.Next(-10, 10) * random.NextDouble(),
+                random.Next(-10, 10) * random.NextDouble(),
+                random.Next(-10, 10) * random.NextDouble()
+            );
+        }
+
+        return formulas;
+    }
+
+    private static void OutputArray(Formula[] formulas)
+    {
+        Console.WriteLine("Formulas:");
+        foreach (var formula in formulas)
+        {
+            Console.WriteLine(formula);
+        }
+
+        Console.WriteLine();
+    }
+
+    private static (double, double, double) InputRange()
+    {
+        Console.Write("Input a: ");
+        var a = Convert.ToDouble(Console.ReadLine());
+        
+        Console.Write("Input b: ");
+        var b = Convert.ToDouble(Console.ReadLine());
+        
+        Console.Write("Input eps: ");
+        var eps = Convert.ToDouble(Console.ReadLine());
+        
+        return (a, b, eps);
+    }
+
+    private static Formula FindMinFormula(Formula[] formulas, double a, double b, double eps)
+    {
+        Formula min = null;
+        double minValue = 0;
+        
+        foreach (var formula in formulas)
+        {
+            for (var x = a; x <= b; x += eps)
+            {
+                var value = formula.GetValue(x);
+                
+                if (min == null)
+                {
+                    min = formula;
+                    minValue = value;
+                }
+                else
+                {
+                    if (minValue <= value) continue;
+                    
+                    min = formula;
+                    minValue = value;
+                }
+            }
+        }
+
+        return min;
+    }
+}

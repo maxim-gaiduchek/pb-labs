810dd5250431f70e4ab01da5599034def0279d8b
maxim-gaiduchek <maxim.gayduchek@gmail.com>
2022-04-07 18:30:02 +0300

    Створено код на C#

---
+++ CSharpProject/CSharpProject/Matrix.cs
@@ -0,0 +1,107 @@
+﻿namespace CSharpProject;
+
+public class Matrix
+{
+    public Matrix(int n)
+    {
+        var random = new Random();
+        var matrix = new int[n][];
+
+        for (var i = 0; i < n; i++)
+        {
+            matrix[i] = new int[n];
+
+            for (var j = 0; j < n; j++)
+            {
+                matrix[i][j] = random.Next(0, 10);
+            }
+        }
+
+        Coefficients = matrix;
+    }
+
+    public Matrix(int[][] matrix)
+    {
+        Coefficients = matrix;
+    }
+
+    private int[][] Coefficients { get; }
+
+    public int GetNorm() // m-norm, by rows
+    {
+        var result = 0;
+
+        for (var i = 0; i < Coefficients.GetLength(0); i++)
+        {
+            var sum = Coefficients[i].Sum();
+
+            if (i == 0 || result < sum)
+            {
+                result = sum;
+            }
+        }
+
+        return result;
+    }
+
+    public static Matrix operator +(Matrix matrix0, Matrix matrix1)
+    {
+        var n = matrix0.Coefficients.GetLength(0);
+        var result = new Matrix(new int[n][]);
+
+        for (var i = 0; i < n; i++)
+        {
+            result.Coefficients[i] = new int[n];
+            
+            for (var j = 0; j < n; j++)
+            {
+                result.Coefficients[i][j] = matrix0.Coefficients[i][j] + matrix1.Coefficients[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    public static Matrix operator *(Matrix matrix0, Matrix matrix1)
+    {
+        var n = matrix0.Coefficients.GetLength(0);
+        var result = new Matrix(new int[n][]);
+
+        for (var i = 0; i < n; i++)
+        {
+            result.Coefficients[i] = new int[n];
+            
+            for (var j = 0; j < n; j++)
+            {
+                result.Coefficients[i][j] = 0;
+
+                for (var a = 0; a < n; a++)
+                {
+                    result.Coefficients[i][j] += matrix0.Coefficients[i][a] * matrix1.Coefficients[a][j];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public override string ToString()
+    {
+        var result = "";
+        var n = Coefficients.GetLength(0);
+
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                result += Coefficients[i][j] + "\t";
+            }
+
+            result += "\n";
+        }
+
+        result += "\n";
+
+        return result;
+    }
+}
---
+++ CSharpProject/CSharpProject/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace CSharpProject;
+
+class Program
+{
+    public static void Main()
+    {
+        var random = new Random();
+        var n = random.Next(2, 6);
+        var matrix = new int[n][];
+        
+        for (var i = 0; i < n; i++)
+        {
+            matrix[i] = new int[n];
+            
+            for (var j = 0; j < n; j++)
+            {
+                matrix[i][j] = random.Next(0, 10);
+            }
+        }
+
+        Matrix matrix1 = new(n), matrix2 = new(matrix);
+
+        Console.WriteLine("M1:");
+        Console.WriteLine(matrix1);
+        Console.WriteLine("M2:");
+        Console.WriteLine(matrix2);
+
+        var matrix3 = matrix1 + matrix2;
+
+        Console.WriteLine("M3:");
+        Console.WriteLine(matrix3);
+
+        matrix3 *= matrix3;
+
+        Console.WriteLine("New M3:");
+        Console.WriteLine(matrix3);
+
+        Console.WriteLine("New M3's norm:");
+        Console.WriteLine(matrix3.GetNorm());
+    }
+}
